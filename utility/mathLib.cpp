638c941fb95b1087aaae1da28c6fc5c4b4f55721
HoangPi <voduchoangphi@gmail.com>
2025-09-12 15:41:09 +0700

    greedy algorithm

--- utility/mathLib.cpp
+++ utility/mathLib.cpp
@@ -1,8 +1,54 @@
 #include "mathLib.h"
 
-int DistanceSquare(Point a, Point b)
+inline bool ExistInVector(std::vector<int> &vec, int item)
 {
-    const int deltaX = (a.x - b.x);
-    const int deltaY = (a.y - b.y);
+    for (auto &&v : vec)
+    {
+        if (v == item)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+long long DistanceSquare(Point a, Point b)
+{
+    const long long deltaX = (a.x - b.x);
+    const long long deltaY = (a.y - b.y);
     return deltaX * deltaX + deltaY * deltaY;
+}
+
+void GreedySolve(std::vector<Vertex> &vertices, std::vector<WeightedUndirectedEdge> &result)
+{
+    result.clear();
+    result.reserve(vertices.size() - 1);
+    std::vector<int> visited;
+    visited.reserve(vertices.size());
+    visited.push_back(0);
+    int current = 0;
+    while (visited.size() < visited.capacity())
+    {
+        double minDistance = 0x7fffffffffffffff;
+        int next = -1;
+        for (int i = 0; i < vertices.size(); i++)
+        {
+            if (!ExistInVector(visited, i))
+            {
+                const double temp = DistanceSquare(vertices[current].p, vertices[i].p);
+                if (temp < minDistance)
+                {
+                    next = i;
+                }
+            }
+        }
+        if(next == -1)
+        {
+            result.emplace_back(&vertices[current], &vertices[0]);
+            return;
+        }
+        visited.push_back(next);
+        result.emplace_back(&vertices[current], &vertices[next]);
+        current = next;
+    }
 }
--- utility/mathLib.h
+++ utility/mathLib.h
@@ -1,7 +1,12 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
+#include "classes/Edge.h"
+#include "classes/Vertex.h"
+#include <vector>
 
 using namespace cv;
 
-int DistanceSquare(Point a, Point b);
+long long DistanceSquare(Point a, Point b);
+
+void GreedySolve(std::vector<Vertex> &vertices, std::vector<WeightedUndirectedEdge> &result);
