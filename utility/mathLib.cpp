37bc63e258a1e42168730c4ba1894689b6e6f9fd
HoangPi <voduchoangphi@gmail.com>
2025-09-12 13:59:36 +0700

    commit

--- utility/mathLib.cpp
+++ utility/mathLib.cpp
@@ -6,3 +6,30 @@ int DistanceSquare(Point a, Point b)
     const int deltaY = (a.y - b.y);
     return deltaX * deltaX + deltaY * deltaY;
 }
+
+void GreedyAlgorithm(vector<Vertex> &Vertices, vector<Edge> &Edges)
+{
+    vector<int> visited;
+    visited.reserve(Vertices.size());
+    Edges.clear();
+    Edges.reserve(Vertices.size());
+    for (int i = 0; i < Vertices.size(); i++)
+    {
+        int minDistance = 0x7fffffff;
+        Vertex *nextVertex = nullptr;
+        for (int j = i + 1; j < Vertices.size(); j++)
+        {
+            int temp = DistanceSquare(Vertices[i].p, Vertices[j].p);
+            if (temp < minDistance)
+            {
+                minDistance = temp;
+                nextVertex = &Vertices[j];
+            }
+        }
+        if(!nextVertex)
+        {
+            exit(-1);
+        }
+        Vertex::MakeEdge(Vertices[i], *nextVertex);
+    }
+}
--- utility/mathLib.h
+++ utility/mathLib.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
+#include "classes/Vertex.h"
 
 using namespace cv;
 
 int DistanceSquare(Point a, Point b);
+
+void GreedyAlgorithm(vector<Vertex> &Vertices, vector<Edge> &Edges);
