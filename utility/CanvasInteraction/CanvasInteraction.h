c9ec44d0b2e58e8145f8f5ef82f76886d2c1b2c7
LAV <voduchoangphi@gmail.com>
2025-09-16 16:30:23 +0700

    implement delete edges

--- utility/CanvasInteraction/CanvasInteraction.h
+++ utility/CanvasInteraction/CanvasInteraction.h
@@ -1,5 +1,12 @@
 #include <opencv2/opencv.hpp>
+#include <vector>
+#include "../classes/Edge.h"
+#include "../classes/Vertex.h"
+#include "../mathLib.h"
 
 using namespace cv;
 
 void HighlightVertex(Point p, Mat &canvas, const cv::String &winname = "White Screen");
+
+// Delete edges by redraw vertices without edges
+void DeleteEdge(std::vector<WeightedUndirectedEdge> &Edges, std::vector<Vertex> Vertices, cv::Mat &canvas, const cv::String &winname);
