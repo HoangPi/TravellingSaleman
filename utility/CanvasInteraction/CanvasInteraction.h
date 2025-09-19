760736d9ea0ee31aeda50f8e11946fce48eefab9
LAV <voduchoangphi@gmail.com>
2025-09-19 13:57:18 +0700

    update implementation to use graph instead

--- utility/CanvasInteraction/CanvasInteraction.h
+++ utility/CanvasInteraction/CanvasInteraction.h
@@ -3,12 +3,13 @@
 #include "../classes/Edge.h"
 #include "../classes/Vertex.h"
 #include "../mathLib.h"
+#include "../classes/Graph.h"
 
 using namespace cv;
 
 void HighlightVertex(Point p, Mat &canvas, const cv::String &winname = "White Screen");
 
 // Delete edges by redraw vertices without edges
-void DeleteEdge(std::vector<WeightedUndirectedEdge> &Edges, std::vector<Vertex> Vertices, cv::Mat &canvas, const cv::String &winname);
+void DeleteEdge(Graph &graph, cv::Mat &canvas, const cv::String &winname);
 void DisplaySingleEdge(const char *const WindowName, Mat &canvas, const WeightedUndirectedEdge &edge, const int offset);
 double DisplayEdges(const char *const WindowName, Mat &canvas, std::vector<WeightedUndirectedEdge> &edges, const int WaitTime = 100, const int offset = 15);
--- utility/mathLib.h
+++ utility/mathLib.h
@@ -1,8 +1,7 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
-#include "classes/Edge.h"
-#include "classes/Vertex.h"
+#include "classes/Graph.h"
 #include "christofides/christofides.h"
 #include <vector>
 
@@ -18,5 +17,5 @@ long long DistanceSquare(Point a, Point b);
 
 cv::Point PointAtDistance(const cv::Point& a, const cv::Point& b, double n);
 
-void NearestNeighbor(std::vector<Vertex> &vertices, std::vector<WeightedUndirectedEdge> &result);
-void Solve(std::vector<Vertex> &vertices, std::vector<WeightedUndirectedEdge> &result, ESOLVE_TYPE Type = NEAREST_NEIGBOR);
+void NearestNeighbor(Graph &graph);
+void Solve(Graph &graph, ESOLVE_TYPE Type = NEAREST_NEIGBOR);
