e48a3c0faa86c42bc896de91d18bfa76425c2431
LAV <voduchoangphi@gmail.com>
2025-09-19 16:21:56 +0700

    update constructor

--- utility/classes/Graph.cpp
+++ utility/classes/Graph.cpp
@@ -5,12 +5,22 @@ Graph::Graph(std::vector<Vertex> &Vertices, std::vector<WeightedUndirectedEdge> 
     const Vertex *oldBase = &Vertices[0];
     auto FindIndex = [oldBase](Vertex *v)
     { return (v - oldBase); };
-    this->Vertices = Vertices;
+
+    this->Vertices.reserve(Vertices.size());
+    this->Edges.reserve(Edges.size());
+
+    for (auto &&v : Vertices)
+    {
+        cv::Point p = v.p;
+        this->Vertices.emplace_back(p);
+    }
+    
+
     for (auto &&oldEdge : Edges)
     {
         int v1Index = FindIndex(oldEdge.v1);
         int v2Index = FindIndex(oldEdge.v2);
-        this->Edges.emplace_back(&this->Vertices[v1Index], &this->Vertices[v2Index]);
+        this->AddEdges(&this->Vertices[v1Index], &this->Vertices[v2Index]);
     }
     if (!IsValid())
     {
@@ -43,5 +53,8 @@ void Graph::AddEdges(Vertex *v1, Vertex *v2)
     if (this->IsValid(v1) && this->IsValid(v2))
     {
         this->Edges.emplace_back(v1, v2);
+        auto lastEdge = this->Edges.back();
+        v1->ConnectedEdges.push_back(&lastEdge);
+        v2->ConnectedEdges.push_back(&lastEdge);
     }
 }
